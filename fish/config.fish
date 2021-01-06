5a695beb9a3563ac800904f3dc00fbeec1b04389
Shempignon <nicolas@pointndots.com>
2021-01-06 15:25:28 +0100

    Big bang

---
+++ fish/config.fish
@@ -0,0 +1,33 @@
+export LC_CTYPE=en_US.UTF-8
+export LC_ALL=en_US.UTF-8
+
+export XDG_CONFIG_HOME=$HOME/.config
+export XDG_CACHE_HOME=$HOME/.cache
+export XDG_DATA_HOME=$HOME/.local/share
+
+export XINITRC="$XDG_CONFIG_HOME"/X11/xinitrc
+export XSERVERRC="$XDG_CONFIG_HOME"/X11/xserverrc
+
+export GOPATH=(go env GOPATH)
+
+set -Ua fish_user_paths (go env GOPATH)/bin
+set -Ua fish_user_paths $HOME/.cargo/bin
+set -Ua fish_user_paths $HOME/.npm-global/bin
+
+alias g="git"
+alias c="cargo"
+alias d="docker-compose run --rm command"
+alias k="kubectl"
+alias gst="git status"
+alias dup="docker-compose up --detach"
+alias dwn="docker-compose down --remove-orphans"
+alias cat="bat"
+alias vi="nvim"
+alias vim="nvim"
+alias l="exa --long --group --time-style long-iso --header --all"
+alias ll="exa --long --group --time-style long-iso --header --all"
+alias k="kubectl"
+alias wm='startx "$XDG_CONFIG_HOME/X11/xinitrc"'
+
+starship init fish | source
+
---
+++ fish/functions/fzf_key_bindings.fish
@@ -0,0 +1,1 @@
+/home/shempi/.fzf/shell/key-bindings.fish
