31dc744e8dad60c2ef102cbae67931f52d5f691c
Shempignon <nicolas@pointndots.com>
2022-06-21 21:36:54 +0200

    20220622

--- config/fish/config.fish
+++ config/fish/config.fish
@@ -1,23 +1,20 @@
 set -gx EDITOR nvim
-set -gx TERMINAL alacritty
+set -gx TERMINAL wezterm
 set -gx LC_CTYPE en_US.UTF-8
 set -gx LC_ALL en_US.UTF-8
-set -gx _JAVA_AWT_WM_NONREPARENTING 1
+set -gx _JAVA_AWT_VM_NONPARENTING 1
 
 set -U fish_greeting
 
 alias g="git"
 alias c="cargo"
-alias d="docker-compose run --rm command"
-alias k="kubectl"
-alias gst="git status"
-alias dup="docker-compose up --detach"
-alias dwn="docker-compose down --remove-orphans"
-alias cat="bat"
+alias v="nvim"
 alias vi="nvim"
 alias vim="nvim"
+alias cat="bat"
+alias gst="git status"
 alias l="exa --long --group --time-style long-iso --header --all"
 alias ll="exa --long --group --time-style long-iso --header --all"
 
-
 starship init fish | source
+fnm env --use-on-cd | source
---
+++ config/wezterm/wezterm.lua
@@ -0,0 +1,9 @@
+local wezterm = require 'wezterm';
+
+return {
+	font = wezterm.font('Iosevka'),
+	default_cursor_style = "BlinkingBar",
+	hide_tab_bar_if_only_one_tab = true,
+	window_background_opacity = 0.95,
+	color_scheme = "wezterm_tokyonight_storm",
+}
