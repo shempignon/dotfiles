484d6cdc5291844bc7fa5b8b03d841d11b8505e7
Shempignon <nicolas@pointndots.com>
2022-09-20 15:02:18 +0200

    20220921

--- config/fish/config.fish
+++ config/fish/config.fish
@@ -1,8 +1,5 @@
 set -gx EDITOR nvim
 set -gx TERMINAL wezterm
-set -gx LC_CTYPE en_US.UTF-8
-set -gx LC_ALL en_US.UTF-8
-set -gx _JAVA_AWT_VM_NONPARENTING 1
 
 set -U fish_greeting
 
@@ -15,6 +12,7 @@ alias cat="bat"
 alias gst="git status"
 alias l="exa --long --group --time-style long-iso --header --all"
 alias ll="exa --long --group --time-style long-iso --header --all"
+alias dup="docker compose up --detach --remove-orphans"
 
 starship init fish | source
 fnm env --use-on-cd | source
